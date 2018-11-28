d8283920ff370839e0f32364ae4a56e55b01f666
Raul <byraul0209@gmail.com>
2018-11-28 14:27:50 -0600

    Funcionalidad completa tabla Factura

--- src/app/components/categoria/categoria.component.ts
+++ src/app/components/categoria/categoria.component.ts
@@ -7,22 +7,24 @@ import { Categoria } from './../interfaces/categoria.interface';
   templateUrl: './categoria.component.html',
   styles: []
 })
+
 export class CategoriaComponent implements OnInit {
   categorias: any[] = [];
   loading = false;
   constructor(private _categoriaService: CategoriaService) {
     this.loading = true;
-    this._categoriaService.getCategorias().subscribe( (data: any) => {
+    this._categoriaService.getCategorias().subscribe((data: any) => {
       this.categorias = data;
       this.loading = false;
     });
   }
   eliminar(index: number) {
     const registro = this.categorias[index];
-    this._categoriaService.deleteCategoria(registro.codigoCategoria).subscribe((data) => {
-        this.categorias.splice(index, index + 1);
+      this._categoriaService.deleteCategoria(registro.codigoCategoria).subscribe((data) => {
+      this.categorias.splice(index, index + 1);
     });
   }
+
   ngOnInit() {
   }
 
