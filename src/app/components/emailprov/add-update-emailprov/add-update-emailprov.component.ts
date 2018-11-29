19245fbaf62f5e2ddf392d065101ca7331fea343
JulioBlas <juliroblaslo@gmail.com>
2018-11-28 22:59:35 -0600

    AddUpdate Email y Telefono

--- src/app/components/emailprov/add-update-emailprov/add-update-emailprov.component.ts
+++ src/app/components/emailprov/add-update-emailprov/add-update-emailprov.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
-
+import { EmailProveedor } from '../../interfaces/emailProveedor.interface';
+import { NgForm } from '@angular/forms';
+import { EmailProveedorService } from './../../../services/email-proveedor.service';
+import { Router, ActivatedRoute } from '@angular/router';
 @Component({
   selector: 'app-add-update-emailprov',
   templateUrl: './add-update-emailprov.component.html',
@@ -7,9 +10,40 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AddUpdateEmailprovComponent implements OnInit {
 
-  constructor() { }
+  emailProveedor: EmailProveedor = {
+    codigoEmail: 0,
+    descripcion: '',
+    email: '',
+    codigoProveedor: 0
+  };
+  nuevo = false;
+  constructor(private _activatedRoute: ActivatedRoute, private _emailprovService: EmailProveedorService, private _router: Router) {
+    this._activatedRoute.params.subscribe(params => {
+      if ( params['id'] > 0) {
+        this._emailprovService.getEmailProveedor(params['id']).subscribe((data: any) => {
+          this.emailProveedor = data;
+        });
+      } else {
+        this.nuevo = true;
+      }
+    });
+   }
 
   ngOnInit() {
+  }
+
+  guardar() {
+    if ( this.nuevo ) {
+      this._emailprovService.addEmailProveedor(this.emailProveedor).subscribe(data => {
+        console.log(data);
+        this._router.navigate(['/emailprov']);
+      });
+    } else {
+      this._emailprovService.updateEmailProveedor(this.emailProveedor).subscribe(data => {
+        console.log(data);
+        this._router.navigate(['/emailprov']);
+      });
+    }
   }
 
 }
--- src/app/components/telprov/add-update-telprov/add-update-telprov.component.ts
+++ src/app/components/telprov/add-update-telprov/add-update-telprov.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
-
+import { TelefonoProveedor } from '../../interfaces/telefonoProveedor.interface';
+import { NgForm } from '@angular/forms';
+import { TelefonoProveedorService } from './../../../services/telefono-proveedor.service';
+import { Router, ActivatedRoute } from '@angular/router';
 @Component({
   selector: 'app-add-update-telprov',
   templateUrl: './add-update-telprov.component.html',
@@ -7,9 +10,40 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AddUpdateTelprovComponent implements OnInit {
 
-  constructor() { }
+  telefonoProveedor: TelefonoProveedor = {
+    codigoTelefono: 0,
+    descripcion: '',
+    numeroTelefono: 0,
+    codigoProveedor: 0
+  };
+  nuevo = false;
+  constructor(private _activatedRoute: ActivatedRoute, private _telprovService: TelefonoProveedorService, private _router: Router) {
+    this._activatedRoute.params.subscribe(params => {
+      if ( params['id'] > 0) {
+        this._telprovService.getTelefonoProveedor(params['id']).subscribe((data: any) => {
+          this.telefonoProveedor = data;
+        });
+      } else {
+        this.nuevo = true;
+      }
+    });
+   }
 
   ngOnInit() {
+  }
+
+  guardar() {
+    if ( this.nuevo ) {
+      this._telprovService.addTelefonoProveedor(this.telefonoProveedor).subscribe(data => {
+        console.log(data);
+        this._router.navigate(['/telprov']);
+      });
+    } else {
+      this._telprovService.updateTelefonoProveedor(this.telefonoProveedor).subscribe(data => {
+        console.log(data);
+        this._router.navigate(['/telprov']);
+      });
+    }
   }
 
 }
